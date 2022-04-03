1aa74424991b5f941539bb8ca50a0be4bf55a7c6
markliang <liang840307@gmail.com>
2022-04-03 20:56:16 +0800

    feat: add the all tables

--- counting_backend/models.py
+++ counting_backend/models.py
@@ -17,3 +17,67 @@ class drinking(models.Model):
     consume_item = models.CharField(max_length=50,blank=True,null=True)
     consume_price = models.CharField(max_length=50,blank=True,null=True)
     consume_date = models.DateField(blank=True,null=True)
+    
+class taxing(models.Model):
+    consume_item = models.CharField(max_length=50,blank=True,null=True)
+    consume_price = models.CharField(max_length=50,blank=True,null=True)
+    consume_date = models.DateField(blank=True,null=True)
+
+class traffic(models.Model):
+    consume_item = models.CharField(max_length=50,blank=True,null=True)
+    consume_price = models.CharField(max_length=50,blank=True,null=True)
+    consume_date = models.DateField(blank=True,null=True)
+class utility_bill(models.Model):
+    consume_item = models.CharField(max_length=50,blank=True,null=True)
+    consume_price = models.CharField(max_length=50,blank=True,null=True)
+    consume_date = models.DateField(blank=True,null=True)
+
+class clothes(models.Model):
+    consume_item = models.CharField(max_length=50,blank=True,null=True)
+    consume_price = models.CharField(max_length=50,blank=True,null=True)
+    consume_date = models.DateField(blank=True,null=True)
+
+class phonefee(models.Model):
+    consume_item = models.CharField(max_length=50,blank=True,null=True)
+    consume_price = models.CharField(max_length=50,blank=True,null=True)
+    consume_date = models.DateField(blank=True,null=True)
+
+
+class health(models.Model):
+    consume_item = models.CharField(max_length=50,blank=True,null=True)
+    consume_price = models.CharField(max_length=50,blank=True,null=True)
+    consume_date = models.DateField(blank=True,null=True)
+
+
+class sport(models.Model):
+    consume_item = models.CharField(max_length=50,blank=True,null=True)
+    consume_price = models.CharField(max_length=50,blank=True,null=True)
+    consume_date = models.DateField(blank=True,null=True)
+
+
+class beauth(models.Model):
+    consume_item = models.CharField(max_length=50,blank=True,null=True)
+    consume_price = models.CharField(max_length=50,blank=True,null=True)
+    consume_date = models.DateField(blank=True,null=True)
+
+
+class gift(models.Model):
+    consume_item = models.CharField(max_length=50,blank=True,null=True)
+    consume_price = models.CharField(max_length=50,blank=True,null=True)
+    consume_date = models.DateField(blank=True,null=True)
+class social(models.Model):
+    consume_item = models.CharField(max_length=50,blank=True,null=True)
+    consume_price = models.CharField(max_length=50,blank=True,null=True)
+    consume_date = models.DateField(blank=True,null=True)
+
+
+class stock(models.Model):
+    consume_item = models.CharField(max_length=50,blank=True,null=True)
+    consume_price = models.CharField(max_length=50,blank=True,null=True)
+    consume_date = models.DateField(blank=True,null=True)
+
+
+class save_money(models.Model):
+    consume_item = models.CharField(max_length=50,blank=True,null=True)
+    consume_price = models.CharField(max_length=50,blank=True,null=True)
+    consume_date = models.DateField(blank=True,null=True)
