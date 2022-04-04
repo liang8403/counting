739d755787aefa5655964b0251f2c137ceb79bf4
markliang <liang840307@gmail.com>
2022-04-04 22:20:37 +0800

    feat:add the consume function

--- counting_backend/views.py
+++ counting_backend/views.py
@@ -6,19 +6,28 @@ from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 @csrf_exempt
 def hello(request):
-    ccc = request.POST['username']
-    bbb = request.POST['password']
+    ccc = request.POST.get('username')
+    bbb = request.POST.get('password')
     output_json = {
         "status":"success",
         "data":"",
         "user":"",
         "password":""
     }
-    print(request.POST)
     aaa = list(eating.objects.all().values())
     output_json['data'] = aaa
     output_json['user'] = ccc
     output_json['password'] = bbb
     return JsonResponse(output_json) 
+@csrf_exempt
 def insert_a_consume(request):
-    return True
+    output_json = {
+        "status":"",
+        "data":""
+    }
+    consume_item = request.POST.get('consume_item')
+    consume_price = request.POST.get('consume_price')
+    consume_category = request.POST.get('consume_category')
+    consume_date = request.POST.get('consume_date')
+    
+    return JsonResponse(output_json)
